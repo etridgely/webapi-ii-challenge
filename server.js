abb2b504a7a06c12bd9fb91b306ea489d9a8b435
etridgely <5757423+etridgely@users.noreply.github.com>
2019-11-07 18:43:45 -0800

    added index and server files

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const hubsRouter= require('./data/db');
+
+const server = express();
+server.use(express.json());
+
+server.use('/api/comments', hubsRouter);
+server.use('/api/messages', hubsRouter);
+
+server.get('/', (req, res) => {
+res.send(`
+    <h2>Message API</h>
+    <p>Welcome to my API</p>
+  `);
+})
+
+module.exports = server;
